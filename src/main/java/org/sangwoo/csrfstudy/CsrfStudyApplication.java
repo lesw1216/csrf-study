98538825e33deb083d220f2a354c488e9021a502
sangwoo <lesw1216@gmail.com>
2026-03-12 03:12:00 +0900

    chore: init

---
+++ src/main/java/org/sangwoo/csrfstudy/CsrfStudyApplication.java
@@ -0,0 +1,13 @@
+package org.sangwoo.csrfstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsrfStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sangwoo/csrfstudy/CsrfStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sangwoo.csrfstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrfStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
